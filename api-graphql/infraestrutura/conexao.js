44a00a93561dc1fe286c3ed797f2b50f01865f9c
Rodrigo Erickson da Silva <roerickson@uolinc.com>
2021-06-02 10:18:58 -0300

    projeto incial api-graphql

--- api-graphql/infraestrutura/conexao.js
+++ api-graphql/infraestrutura/conexao.js
@@ -4,8 +4,9 @@ const conexao = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '',
-  database: 'agenda-petshop'
+  password: 'admin',
+  database: 'agenda-petshop',
+  multipleStatements: true
 })
 
 module.exports = conexao
